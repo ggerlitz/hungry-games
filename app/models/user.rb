0b6244a31d1a7ea978a5795006a3d688dd800e06
Brian Fountain <brian@brianfountain.com>
2015-09-21 15:35:18 -0400

    Add profile views

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,8 @@ class User < ActiveRecord::Base
 	validates_presence_of :first_name, :last_name, :email, :password, :password_confirmation, :address
 	validates_uniqueness_of :email
 	validates_confirmation_of :password
+
+	def full_name
+		"#{first_name} #{last_name}"
+	end
 end
